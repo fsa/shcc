8a0f567ad405b82518bbf79e8b7697a60bd73932
Sergei Fedotov <fsa@tavda.info>
2024-01-13 05:34:50 +0700

    API: настройки демонов

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,26 +2,31 @@
 
 namespace App\Controller;
 
-use ShccFramework\Plugins;
+use ShccFramework\PluginRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ApiController extends AbstractController
 {
     #[Route('/api/daemon/{name}')]
-    function getDaemon(string $name, Plugins $plugins): Response
+    function getDaemon(string $name, PluginRepository $pluginRepository, ParameterBagInterface $parameterBag): Response
     {
-        $plugin_info = $plugins->getPlugin($name);
-        if (!$plugin_info) {
+        $plugin_info = $pluginRepository->find($name);
+        if ($plugin_info === null) {
             return $this->json(['error' => 'Plugin not found'], 400);
         }
-        if (!isset($plugin_info->daemon)) {
+        $daemon = $plugin_info->getDaemon();
+        if ($daemon === null) {
             return $this->json(['error' => 'Plugin Daemon not found'], 400);
         }
+        $default_settings = $plugin_info->getSettings();
+        $settings = $parameterBag->has('shcc.daemon.'.$name)? $parameterBag->get('shcc.daemon.' . $name):[];
+
         return $this->json([
-            'daemon' => $plugin_info->daemon,
-            'settings' => isset($plugin_info->settings) ? array_merge((array)$plugin_info->settings, []) : []
+            'daemon' => $daemon,
+            'settings' => $settings ? array_merge($default_settings, $settings) : $default_settings
         ]);
     }
 }
